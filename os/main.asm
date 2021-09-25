125d7875af0c943dadd7205b74b8da4652006c24
c0nny <caojunatthu@163.com>
2021-09-25 09:37:52 +0800

    V0.0.2 assembly code: use loop

--- os/main.asm
+++ os/main.asm
@@ -1,63 +1,22 @@
 #main.asm
-addi a0, zero, 72 # 设置打印字符的参数: H
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 # 打印字符的操作吗
-ecall #发起调用，陷入机器模式（M模式），调用BIOS提供的打印中断
-addi a0, zero, 101 # 设置打印字符的参数: e
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 108 # 设置打印字符的参数: l
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 108 # 设置打印字符的参数: l
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 111 # 设置打印字符的参数: o
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 44 # 设置打印字符的参数: ,
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 119 # 设置打印字符的参数: w
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 111 # 设置打印字符的参数: o
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 114 # 设置打印字符的参数: r
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 108 # 设置打印字符的参数: l
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 100 # 设置打印字符的参数: d
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-addi a0, zero, 33 # 设置打印字符的参数: !
-addi a1, zero, 0 
-addi a2, zero, 0 
-addi a7, zero, 1 
-ecall 
-auipc a0, 0  #保存当前指令的PC到a0。auipc就是add immediate to pc
-jalr  ra, a0, 0 #死循环
+    .section .text
+    .globl _start  #这个符号需要声明，链接器是需要导出到ELF中的
+_start:
+    la t0, message # 加载当前要打印的字符的地址，伪指令la，展开就是addi
+    lb t1, 0(t0) # 加载当前要打印的字符，这是读内存，一个byte
+putchar: #定义一个局部的符号，表示当前指令的地址（也就是下一条mv）
+    mv a0, t1 # 设置打印字符的参数
+    mv a1, zero #未使用
+    mv a2, zero #未使用
+    addi a7, zero, 1 # 打印字符的操作吗
+    ecall 
+    addi t0, t0, 1 #地址+1
+    lb t1, 0(t0) # 当前要打印的字符
+    bne t1, zero, putchar # 如果当前字符不是空字符串（结束符），则跳转到putchar位置
+dead_loop:
+    jal        dead_loop  #死循环，也是伪指令
+
+    .section .rodata  #定义在readonlydata这个段，这是惯例，Hello world字符串就是常量
+message: #message这个字符串的开始位置
+    .string "Hello, world!"  #会自动添加一个\0的结束符
+    
