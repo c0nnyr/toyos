3f1485e3f75dde9d4fb8cbe555fd1b0647aa36fe
c0nny <caojunatthu@163.com>
2021-09-25 14:07:49 +0800

    V0.0.3 Hello world rust: bare hello world

--- os/src/main.rs
+++ os/src/main.rs
@@ -7,4 +7,14 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
 }
 
 #[no_mangle]
-fn _start() {}
+fn _start() -> ! {
+    let s = "Hello, world!";
+    for ch in s.chars() {
+        print_char(ch);
+    }
+    loop {} //避免代码往下走走到了未知的指令
+}
+
+fn print_char(ch: char) {
+    //TODO print a characture
+}
