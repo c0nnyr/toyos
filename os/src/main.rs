27148d1466924de97fe054b84de823e8129d32aa
caojun.08 <caojun.08@bytedance.com>
2021-09-30 13:32:35 +0800

    V0.0.4 print: kprint

--- os/src/main.rs
+++ os/src/main.rs
@@ -9,12 +9,23 @@ fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
+macro_rules! kprint {
+    ($($arg:tt)*) => {core::fmt::write(&mut SerialIO{}, core::format_args!($($arg)*)).unwrap()}
+}
+
+struct SerialIO {}
+
+impl core::fmt::Write for SerialIO {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for ch in s.chars() {
+            print_char(ch);
+        }
+        Ok(()) //这里是返回值，等价于return Ok(())。OK是Result的一个枚举类型
+    }
+}
 #[no_mangle]
 fn main() -> ! {
-    let s = "Hello, world!";
-    for ch in s.chars() {
-        print_char(ch);
-    }
+    kprint!("Hello, world! {}. Score {}", "Tom", 100);
     loop {} //避免代码往下走走到了未知的指令
 }
 
