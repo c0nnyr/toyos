39c006878cbfd287aee4fe68b0f735b5cd67578b
c0nny <caojunatthu@163.com>
2021-10-21 08:10:12 +0800

    V0.0.8: add _start

--- app/src/bin/hello_world.rs
+++ app/src/bin/hello_world.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![no_main]
 
 use libr::println;
 fn main() {
--- libr/src/lib.rs
+++ libr/src/lib.rs
@@ -1,7 +1,19 @@
 #![no_std]
 #![feature(global_asm)]
+#![feature(linkage)]
 
 mod arch;
 #[macro_use]
 pub mod io;
 mod panic;
+
+#[no_mangle]
+#[link_section = ".text._start"]
+pub fn _start() {
+    main(); // TODO 应该有个退出机制
+}
+
+#[linkage = "weak"]
+fn main() -> usize {
+    panic!("never here");
+}
