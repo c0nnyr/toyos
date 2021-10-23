6a464ea7bbf85871c6f93ff82e76aee74b856dc7
c0nny <caojunatthu@163.com>
2021-10-23 17:26:40 +0800

    V0.0.9: fix task_manager lock bug

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![feature(global_asm)]
 
-use crate::arch::trap::TrapContextStore;
+use crate::arch::trap::{self, TrapContextStore};
 
 #[no_mangle]
 fn main() {
@@ -11,10 +11,13 @@ fn main() {
         .init(log::logger::Level::Info, log::logger::LoggerType::SerialIO);
     arch::trap::init();
     task::task_manager::init();
+    build_first_task_trap_context().restore_trap();
+}
+// 这里必须依靠一个函数，及时释放task_manager获得的锁。
+fn build_first_task_trap_context() -> trap::TrapContext {
     let mut task_manager = task::task_manager::TASK_MANAGER.lock();
     task_manager.switch_to_task(0).unwrap(); //至少得有一个应用
-    let ctx = task_manager.get_default_trap_context();
-    ctx.restore_trap();
+    task_manager.get_default_trap_context()
 }
 #[macro_use]
 mod io; //出现在早点的位置，这样后面的模块就可以直接使用宏了;
